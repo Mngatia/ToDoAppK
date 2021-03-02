6adf7a1425dc3520c87fc51eae3e332eba7fcec6
Mngatia <nyawirahngatia@gmail.com>
2021-03-03 00:15:35 +0300

    ToDo List App KT.

---
+++ app/src/main/java/com/example/todoappk/Converter.kt
@@ -0,0 +1,15 @@
+package com.example.todoappk
+
+import androidx.room.TypeConverter
+import models.Priority
+
+class Converter {
+    @TypeConverter
+    fun fromPriority(priority: Priority): String{
+        return priority.name
+    }
+    @TypeConverter
+    fun toPriority(priority: String): Priority {
+        return Priority.valueOf(priority)
+    }
+}
---
+++ app/src/main/java/models/Priority.kt
@@ -0,0 +1,7 @@
+package models
+
+enum class Priority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
