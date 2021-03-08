d5feb44ce289e66a1ffa8cb738b9ec098ebf1c3c
Mngatia <nyawirahngatia@gmail.com>
2021-03-08 22:20:03 +0300

    ToDo List App KT.

--- app/src/main/java/com/example/todoappk/MainActivity.kt
+++ app/src/main/java/com/example/todoappk/MainActivity.kt
@@ -2,11 +2,21 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import kotlinx.parcelize.Parcelize
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+ //import kotlinx.parcelize.Parcelize
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
